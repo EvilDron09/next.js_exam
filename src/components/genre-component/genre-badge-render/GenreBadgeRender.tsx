f5b957e2eeecb1bf6bb637d11468c6127f36351b
EvilDron09 <andrej32118@gmail.com>
2026-07-28 22:04:12 +0300

    Style finish

--- src/components/genre-component/genre-badge-render/GenreBadgeRender.tsx
+++ src/components/genre-component/genre-badge-render/GenreBadgeRender.tsx
@@ -1,16 +1,20 @@
 import {getGenres} from "@/src/services/api-service";
 import {GenreBadge} from "@/src/components/genre-component/genre-badge/GenreBadge";
 import Link from "next/link";
+import './style/genreBadgeRenderStyle.css'
+
 
 
 export const GenreBadgeRender = async() => {
     const genres = await getGenres()
     return (
-        <ul>
-            {
-                genres.map(genre => <GenreBadge key={genre.id} item={genre}/>)
-            }
-            <li><Link href={'/'}>All Movies</Link></li>
-        </ul>
+        <div className={'genresBadgeRender'}>
+            <ul>
+                {
+                    genres.map(genre => <GenreBadge key={genre.id} item={genre}/>)
+                }
+                <li><Link href={'/'}>All Movies</Link></li>
+            </ul>
+        </div>
     );
 };
--- src/components/genre-component/genre-badge/GenreBadge.tsx
+++ src/components/genre-component/genre-badge/GenreBadge.tsx
@@ -1,14 +1,18 @@
 import {IGenre} from "@/src/models/gener/IGenre";
 import Link from "next/link";
+import './style/genreBangeDtyle.css'
 
 interface IGenreBadge {
     item:IGenre
 }
 
 export const GenreBadge = ({item}:IGenreBadge) => {
+
     return (
-        <li>
-            <Link href={'/genre/'+ item.id.toString()}>{item.name}</Link>
+        <li className={'genresBadge'}>
+            <label htmlFor="genreToggle">
+                <Link href={'/genre/'+ item.id.toString()} >{item.name}</Link>
+            </label>
         </li>
     );
 };
